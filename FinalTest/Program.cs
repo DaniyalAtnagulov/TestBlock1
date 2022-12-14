33e7cbc777a25a780bf4718e6084a87fb014401b
Daniyal~ <danialastrachan@gmail.comgit config --global user.email danialastrachan@gmail.com>
2022-12-14 07:07:01 +0300

    created FilterArray

--- FinalTest/Program.cs
+++ FinalTest/Program.cs
@@ -1,2 +1,18 @@
 ﻿Console.Clear();
 string [] originalarray = new string [9] {"[","10,", "1034,", "555,", "674,", "321,", "8,", "x","]"};
+string [] newarray = new string [originalarray.Length];
+
+
+void FilterArray(string[] firstarray, string[] secondaray)
+{
+    int count = 0;
+    for (int i = 0; i < firstarray.Length; i++)
+    {
+    if(firstarray[i].Length <= 3)
+        {
+        secondaray[count] = firstarray[i];
+        count++;
+        }
+    }
+}
+
